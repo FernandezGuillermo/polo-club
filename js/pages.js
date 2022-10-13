b88ab67b2cd803a460c74fc2ba16b9f157633764
Fernandez Guillermo <fernandezguillermo86@gmail.com>
2022-10-13 19:44:21 -0300

    footer in all pages

--- js/pages.js
+++ js/pages.js
@@ -3,7 +3,7 @@ const navBar = document.getElementById("navBar");
 navBar.innerHTML = `
                     <nav class="navbar navbar-expand-lg bg-color"> <!--NAV BAR-->
                     <div class="container-fluid mt-2">
-                    <a class="navbar-brand" href="#">
+                    <a class="navbar-brand" href="../index.html">
                         <img src="../resources/img-polo-nav.png" alt="Logo Polo" width="30" height="30">
                     </a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -29,5 +29,31 @@ navBar.innerHTML = `
                     </nav>     
                     `
 
+const footer = document.getElementById("footer");
+
+//Create a footer whit bootstrap
+footer.innerHTML = `
+                <article class="row">
+                <div class="col-xl-3 my-auto text-center footer__text">
+                    <p class="footer__text">Folow Us</p>
+                    <a href="https://www.instagram.com/polo.esperanza/?hl=es-la"><img src="../resources/img-instagram.png" alt="instagram" class="footer__instagram" id="icono-instagram"></a>
+                    <a href="https://es-la.facebook.com/poloclubesperanza/"><img src="../resources/img-facebook.png" alt="facebook" class="footer__facebook" id="icono-facebook"></a>
+                </div>
+                <div class="col-xl-6 text-center my-auto">
+                    <p class="footer__text">Contact Us</p>
+                    <p class="footer__text">Lehmann 1339</p>
+                    <p class="footer__text">+54 342 530-4765</p>
+                </div>
+                <div class="col-xl-3 my-auto text-center">
+                    <img src="../resources/img-polo-nav.png" alt="logo marca" class="footer-logo mt-3">
+                    <p class="footer-text-logo text-center">Local exclusivo Oxford Polo Club</p>
+                </div>
+            </article>
+            <article class="row">
+                <div class="float-end">
+                    <p class="footer__text text-center">© Todos los derechos reservados</p>
+                </div>
+            </article>
+`
 
 
