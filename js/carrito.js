36fd07a84fcee9de05b8ec462713201568380f1e
Fernandez Guillermo <fernandezguillermo86@gmail.com>
2022-10-17 17:46:17 -0300

    mods card carrito

--- js/carrito.js
+++ js/carrito.js
@@ -13,7 +13,7 @@ function mostrarCarrito(items){
         card.classList.add("card");
         card.style="width :18rem;"
         card.innerHTML = `
-                        <div class="card mb-3" style="max-width: 540px;">
+                        <div class="card" style="max-width: 600px;">
                             <div class="row g-0">
                             <div class="col-md-4">
                                 <img src="../resources/img-1.png" class="img-fluid rounded-start" alt="...">
@@ -21,7 +21,7 @@ function mostrarCarrito(items){
                             <div class="col-md-8">
                                 <div class="card-body">
                                 <h5 class="card-title">${element.nombre}</h5>
-                                <button type="button" class="btn-close d-block" aria-label="Close"></button>
+                                <button type="button" class="btn-close position-absolute top-0 end-0" aria-label="Close"></button>
                                 <p class="card-text">Talle: ${element.talle}</p>
                                 <p class="card-text">Precio: $${element.precio}</p>
                                 <button class="btn btn-primary">Pagar</button>
